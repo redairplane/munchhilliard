77251fcc59485cb3f65ceac3e2710999a4fad9c1
Matt14c <45790666+Matt14c@users.noreply.github.com>
2020-10-30 10:59:17 -0400

    Update Restaurant.java

--- java-classes/Restaurant.java
+++ java-classes/Restaurant.java
@@ -7,10 +7,8 @@ public class Restaurant
     private int reviewCount;
     private double reviewScoreTotal;
     private String name;
-    private String address;
     // add more
-    private String location;
-
+    private String dishOfTheDay;
     /**
       describe this constructor
     */
@@ -20,28 +18,10 @@ public class Restaurant
         reviewCount = 0;
         reviewScoreTotal = 0;
         name = _name;
-        location = "Not listed";
+      dishOfTheDay = "Unknown";
     }
 
     // accessors and mutators here
-  
-  
-  /**
-      Sets the resturant's address
-      @param String _address
-    */
-    public void setAddress(String _address){
-      address = _address;
-    }
-  
-     /**
-      Returns the resturant's address
-      @return String address which is the address of the resturant
-    */
-  
-    public String getAddress(){
-      return address;
-    }
 
     /**
       describe each method
@@ -53,23 +33,18 @@ public class Restaurant
     }
     
     /**
-      setLocation(String _location)
-      Assigns a specific location to the restaurant.
-      @param _location the location of the restaurant
+    setDishOfTheDay(Sring _dishOfTheDay)
+    Sets the dish of the day
     */
-    public void setLocation(String _location)
-    {
-      location = _location;
-    }
-    
-    /**
-      getLocation()
-      Returns the location of the restaurant.
-      @return the location of the restaurant
-    */
-    public String getLocation()
-    {
-      return location;
+    public void setDishOfTheDay(String _dishOfTheDay){
+      dishOfTheDay = _dishOfTheDay;
     }
   
+    /**
+    getDishOfTheDay()
+    Accesses the dish of the day
+    */
+    public String getDishOfTheDay(){
+      return dishOfTheDay;
+    }
 }
