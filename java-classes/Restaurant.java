267c5c9bcb6fad7e61916c09417a8aeb91672117
KevinChen882645 <57911010+882645@users.noreply.github.com>
2020-10-30 10:54:13 -0400

    Update Restaurant.java

--- java-classes/Restaurant.java
+++ java-classes/Restaurant.java
@@ -8,6 +8,7 @@ public class Restaurant
     private double reviewScoreTotal;
     private String name;
     // add more
+    private String typeOfFood;
 
     /**
       describe this constructor
@@ -18,9 +19,28 @@ public class Restaurant
         reviewCount = 0;
         reviewScoreTotal = 0;
         name = _name;
+      
     }
 
     // accessors and mutators here
+  
+    /**
+      Returns the type of food in the restaurant
+      @return typeOfFood which is the type of food in the restaurant
+    */
+    public String getTypeOfFood()
+    {
+      return typeOfFood;
+    }
+  
+    /**
+      Sets the type of food in the restaurant
+      @param _typeOfFood the type of food in the restaurant
+    */
+    public void setTypeOfFood(String _typeOfFood)
+    {
+      typeOfFood = _typeOfFood;
+    }
 
     /**
       describe each method
